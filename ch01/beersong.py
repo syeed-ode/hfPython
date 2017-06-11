1d22e7898ccead3a1534d91a4ffd4eb0aa59a71f
Syeed Ode <sy_ode@yahoo.com>
2017-06-11 15:37:34 -0400

    Building out python basics

---
+++ ch01/beersong.py
@@ -0,0 +1,8 @@
+word = "bottles"
+
+for beer_num in range(99, 0, -1):
+    print(beer_num, word, "of beer on the wall.")
+    print(beer_num, word, "of beer.")
+    print("Take one down.")
+    print("Pass it around.")
+    if beer_num
--- ch01/odd.py
+++ ch01/odd.py
@@ -1,12 +1,21 @@
 from datetime import datetime
+from os import getcwd
+from random import randint
+from time import sleep
 
 odds = [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19,
         21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
         41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
 
-right_this_minute = datetime.today().minute
+where_am_i = getcwd()
 
-if right_this_minute in odds:
-    print("This minute seems a little odd")
-else:
-    print("Not an odd minute.")
+for i in range(5):
+    right_this_minute = datetime.today().minute
+
+    if right_this_minute in odds:
+        print("This minute seems a little odd. But at least I'm here: " + where_am_i + "_")
+    else:
+        print("Not an odd minute. But at least I'm here: " + where_am_i)
+    wait_time = randint(1,20)
+    print(wait_time)
+    sleep(wait_time)
